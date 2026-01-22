2d1d00e8f06a93a8263da105646fb9e4e08c91ec
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-01-23 00:41:26 +0530

    final

--- frontend/src/app/components/Footer.jsx
+++ frontend/src/app/components/Footer.jsx
@@ -4,16 +4,15 @@ const Footer = () => {
   return (
   
 <footer className="mt-auto bg-[#ED9D05] rounded-tl-[50px] rounded-tr-[50px]">
-  <div className="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 p-8 mx-auto">
+  <div className="mt-auto w-full max-w-[85rem] sm:px-6 p-4 mx-auto">
      {/* Grid */}
-    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
-      <div className="col-span-full lg:col-span-1">
-        <img src="/logo/logo.png" alt="logo" className="h-24 w-auto max-w-[180px] object-contain"/>
-
+    <div className="grid lg:grid-cols-4 gap-6">
+      <div className=" flex justify-center col-span-full lg:col-span-1">
+        <img src="/logo/logo.png" alt="logo" className="h-24  w-auto max-w-[180px] object-contain"/>
       </div>
       {/* End Col */}
 
-      <nav aria-label="Footer Navigation" className="col-span-1">
+      <nav aria-label="Footer Navigation" className=" col-span-1">
         <h4 className="font-bold font-['Libre_Baskerville'] text-center text-xl text-white ">Quick Links</h4>
          <ul className="mt-2 text-center">
         <li><a href='/' className="transition duration-300 hover:scale-90 text-white font-['Libre_Baskerville'] text-lg font-semibold ">Home</a></li>
@@ -25,8 +24,8 @@ const Footer = () => {
       </nav>
       {/* End Col */}
 
-      <nav aria-label="Footer Navigation" className="col-span-1">
-        <h4 className="text-white font-bold font-['Libre_Baskerville'] text-xl">Quick Links</h4>
+      <nav aria-label="Footer Navigation" className=" col-span-1">
+        <h4 className="text-white font-bold font-['Libre_Baskerville'] lg:text-center text-xl">Quick Links</h4>
           <ul className="mt-3 grid">
          <li>
             <a href="#" className="inline-flex items-center transition duration-300 hover:scale-90 gap-1.5"><img src="/socialmedia-icon/whatsapp logo.png" alt="Whatshapp" className="w-10"/> <span className="text-white font-['Libre_Baskerville'] text-lg font-semibold">Whatshapp</span></a>
@@ -48,11 +47,44 @@ const Footer = () => {
           </li>
       </ul>
       </nav>
+ {/* Newsletter */}
+      <div className="col-span-full md:col-span-2 lg:col-span-1">
+  <h4 className="text-white font-bold font-['Libre_Baskerville'] text-xl  mb-1">
+    Newsletter
+  </h4>
+
+  <p className="text-white/90 text-sm mb-3">
+    Subscribe to get SEO tips, updates & tools.
+  </p>
+
+  <form className="flex items-center gap-2">
+
+    <label htmlFor="newsletter-email" className="sr-only">
+      Email address
+    </label>
+
+    <input
+      type="email"
+      id="newsletter-email"
+      required
+      placeholder="Enter your email"
+      className="flex-1 px-4 py-2 rounded-md border border-zinc-700 "
+    />
+
+    <button
+      type="submit"
+      className="bg-white text-md font-bold font-['Libre_Baskerville'] cursor-pointer rounded-[5px] p-2"
+    >
+      Subscribe
+    </button>
+
+  </form>
+</div>
+
+
+
+
     </div>
-      
-    
-    
-   
   </div>
 </footer>
 
